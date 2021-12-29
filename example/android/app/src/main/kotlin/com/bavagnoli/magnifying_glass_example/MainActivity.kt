ff4b762ca895a1c11e569d386926461b01ae561a
Alnitak <lil.deimos@gmail.com>
2021-12-29 09:31:45 +0100

    added windows support

---
+++ example/android/app/src/main/kotlin/com/bavagnoli/magnifying_glass_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bavagnoli.magnifying_glass_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import magnifying_glass
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  MagnifyingGlassPlugin.register(with: registry.registrar(forPlugin: "MagnifyingGlassPlugin"))
+}
